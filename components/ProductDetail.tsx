f2b450a864ef5a5b8d6c2b1f71182155059d8b57
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-03-25 13:54:36 -0400

    Created the Cart to store the items using zustand

--- components/ProductDetail.tsx
+++ components/ProductDetail.tsx
@@ -1,13 +1,32 @@
+"use client";
 import Image from "next/legacy/image";
 import Stripe from "stripe";
 import { Button } from "./ui/button";
+import { useCartStore } from "@/store/CartStore";
 
 interface Props {
     product: Stripe.Product;
 }
 
 export default function ProductDetail({ product }: Props) {
+    const { items, addItem, removeItem } = useCartStore();
     const price = product.default_price as Stripe.Price;
+
+    const cartItem = items.find((item) => {
+        return item.id === product.id;
+    });
+
+    const quantity = cartItem ? cartItem.quantity : 0;
+
+    function onAddItem() {
+        addItem({
+            id: product.id,
+            name: product.name,
+            price: price.unit_amount as number,
+            imageUrl: product.images ? product.images[0] : null,
+            quantity: 1,
+        });
+    }
 
     return (
         <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8 items-center">
@@ -35,9 +54,16 @@ export default function ProductDetail({ product }: Props) {
                 )}
 
                 <div className="flex items-center space-x-4">
-                    <Button variant={"outline"}>-</Button>
-                    <span>0</span>
-                    <Button variant={"outline"}>+</Button>
+                    <Button
+                        variant={"outline"}
+                        onClick={() => removeItem(product.id)}
+                    >
+                        -
+                    </Button>
+                    <span>{quantity}</span>
+                    <Button variant={"outline"} onClick={onAddItem}>
+                        +
+                    </Button>
                 </div>
             </div>
         </div>
